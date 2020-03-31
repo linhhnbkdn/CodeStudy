4bf97833a2e40da9c947e5407a806395ac352ab1
lnhoang <lnhoang@amperecomputing.com>
2020-03-31 11:07:26 +0700

    Add SVC

---
+++ SVC/Readme.md
@@ -0,0 +1,16 @@
+python3 SVC.py
+[INFO]: Training model
+[INFO]: Accurancy on validation data: 0.8144329896907216
+                   precision    recall  f1-score   support
+
+     Ariel Sharon       0.60      0.69      0.64        13
+     Colin Powell       0.84      0.87      0.85        60
+  Donald Rumsfeld       0.66      0.78      0.71        27
+    George W Bush       0.93      0.92      0.92       146
+Gerhard Schroeder       0.79      0.76      0.78        25
+      Hugo Chavez       0.77      0.67      0.71        15
+       Tony Blair       0.88      0.78      0.82        36
+
+         accuracy                           0.85       322
+        macro avg       0.78      0.78      0.78       322
+     weighted avg       0.85      0.85      0.85       32
---
+++ SVC/SVC.py
@@ -0,0 +1,23 @@
+# import the necessary packages
+from __future__ import print_function
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
+from sklearn.datasets import fetch_lfw_people
+from sklearn.model_selection import train_test_split
+
+dataSet = fetch_lfw_people(min_faces_per_person=70)
+
+trainData, testData, trainLabels, testLabels = train_test_split(dataSet.data, dataSet.target, test_size=0.25, random_state=42)
+trainData, valData, trainLabels, valLabels = train_test_split(trainData, trainLabels, test_size=0.1, random_state=42)
+
+print('[INFO]: Training model')
+# model = LogisticRegression(verbose=1, max_iter=1000, tol=1e-3)
+model = SVC(C=1, kernel='poly', verbose=0)
+model.fit(trainData, trainLabels)
+
+score = model.score(valData, valLabels)
+print('[INFO]: Accurancy on validation data: {}'.format(score))
+
+predictions = model.predict(testData)
+report = classification_report(testLabels, predictions, target_names=dataSet.target_names)
+print(report)
